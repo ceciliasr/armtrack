99a79dc7d7df49f288a89076a2be4ddd8b4e9881
ceciliasr <cecilia.siliato@gmail.com>
2020-03-04 20:40:23 +0100

    only R package files

--- Code/Matlab/MotionTrunkDisplay.m
+++
@@ -1,7 +0,0 @@
-%% Motion display with chest
-% This script shows the upper limb joint positions toghether with the
-% tilting and orientation of the trunk segment.
-addpath(genpath('slamToolbox'),'Models',genpath('Data'))
-load VictorModel.mat
-load VictorLandmarks.mat
-uiopen('*.mat')
